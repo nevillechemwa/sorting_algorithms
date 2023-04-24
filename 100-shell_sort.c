e68d542f9e806ae5771e209099e059c2e371a083
nevillechemwa <nevillechemwa@gmail.com>
2023-04-24 07:31:57 -0700

    Shell sort - Knuth Sequence and Cocktail shaker sort algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,36 +1,26 @@
 #include "sort.h"
 
 /**
-  * shell_sort - A function tat sorts an array usin shell algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
+ * @size: size of array
+ */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+	int gap = 1, i, j, tmp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	for (; gap > 0; gap = (gap - 1) / 3)
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (i = gap; i < size; i++)
+		for (i = gap; i < (int)size; i += 1)
 		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
+			tmp = array[i];
 
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = tmp;
 		}
 		print_array(array, size);
 	}
