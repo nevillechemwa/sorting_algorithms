0ea974dce3413c0368fce3ed28f8f1f1ec41cbff
nevillechemwa <nevillechemwa@gmail.com>
2023-04-24 08:16:16 -0700

    sorting

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,11 +1,53 @@
 #include "sort.h"
 
 /**
- * radix_sort -
+ * radix_sort - Sort an array of integers in ascending order using the
+ *              Radix sort algorithm
  *
- * Return: 
+ * @array: Array of integers to sort
+ * @size: Size of the array
  */
 void radix_sort(int *array, size_t size)
 {
+    int *output;
+    int max_value, exp, i;
 
+    if (array == NULL || size < 2)
+        return;
+
+    output = malloc(sizeof(int) * size);
+    if (output == NULL)
+        return;
+
+    max_value = array[0];
+    for (i = 1; i < (int)size; i++)
+    {
+        if (array[i] > max_value)
+            max_value = array[i];
+    }
+
+    for (exp = 1; max_value / exp > 0; exp *= 10)
+    {
+        int count[10] = {0};
+
+        for (i = 0; i < (int)size; i++)
+            count[(array[i] / exp) % 10]++;
+
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (i = size - 1; i >= 0; i--)
+        {
+            output[count[(array[i] / exp) % 10] - 1] = array[i];
+            count[(array[i] / exp) % 10]--;
+        }
+
+        for (i = 0; i < (int)size; i++)
+            array[i] = output[i];
+
+        print_array(array, size);
+    }
+
+    free(output);
 }
+
--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,11 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * bitonic_sort -
- *
- * Return: 
+ * bitonic_merge - Merge two subarrays of array[]
+ * @array: Pointer to array to be sorted
+ * @low: Starting index of the first subarray
+ * @count: Number of elements to be sorted
+ * @dir: Order (UP/DOWN) of sorting
+ */
+void bitonic_merge(int *array, int low, size_t count, int dir)
+{
+    if (count > 1) {
+        int k = count / 2;
+        for (int i = low; i < low + k; i++) {
+            if ((array[i] > array[i + k]) == dir) {
+                int tmp = array[i];
+                array[i] = array[i + k];
+                array[i + k] = tmp;
+            }
+        }
+        bitonic_merge(array, low, k, dir);
+        bitonic_merge(array, low + k, k, dir);
+    }
+}
+
+/**
+ * bitonic_sort - Sorts an array of integers in ascending order
+ * @array: Pointer to array to be sorted
+ * @size: Number of elements in the array
  */
 void bitonic_sort(int *array, size_t size)
 {
-
+    for (int k = 2; k <= size; k *= 2) {
+        for (int j = k / 2; j > 0; j /= 2) {
+            for (int i = 0; i < size; i += k) {
+                bitonic_merge(array, i, j, UP);
+                bitonic_merge(array, i + j, j, DOWN);
+            }
+        }
+        if (k != size) {
+            printf("Merging [%lu/%lu] (UP):\n", size, size);
+            print_array(array, size);
+        }
+    }
 }
+
