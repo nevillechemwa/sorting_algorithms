6bf78440b3501213ca47e21c294cccb97567a2d8
nevillechemwa <nevillechemwa@gmail.com>
2023-04-24 07:46:10 -0700

    Heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,72 +1,57 @@
+#include <stdint.h>
 #include "sort.h"
-
+#define getParent(i) (((i) - 1) / 2)
+#define getLeft(i) (2 * (i) + 1)
+#define getRight(i) (2 * (i) + 2)
 /**
- * heap_sort - Sorting array using heap sort algorithm
- * @array: Array to be sorted
- * @size: Size of the array
- * Return: 0
+ * sift_down - sift_down
+ * @array: array containing heap
+ * @size: total size of array
+ * @index: index of index node of heap
+ * @nth: index of nth node in heap to examine
+ */
+void sift_down(int *array, size_t size, size_t index, size_t nth)
+{
+size_t largest, left, right;
+do {
+left = getLeft(index);
+right = getRight(index);
+largest = index;
+if (right <= nth && array[right] > array[index])
+largest = right;
+if (array[left] > array[largest])
+largest = left;
+if (index == largest)
+return;
+array[index] ^= array[largest];
+array[largest] ^= array[index];
+array[index] ^= array[largest];
+print_array(array, size);
+index = largest;
+} while (getLeft(index) <= nth);
+}
+/**
+ * heap_sort - use heap sort
+ * @array: array to sort
+ * @size: size of array
  */
 void heap_sort(int *array, size_t size)
 {
-	int i;
-
-	if (!array || size < 2)
-		return;
-
-	for (i = size / 2; i >= 0; i--)
-		heapify(array, size, i, size);
-	for (i = size - 1; i >= 0; i--)
-	{
-		swap(&array[i], &array[0]);
-		if (i != 0)
-			print_array(array, size);
-		heapify(array, i, 0, size);
-	}
-}
-
-/**
- * heapify - Recursive function to sort binary tree
- * @array: array to be sorted as binary tree
- * @end: Last node in binary tree
- * @start: First node of binary tree
- * @size: Size of the array to sort
- * Return: 0
- */
-void heapify(int *array, int end, int start, size_t size)
+size_t node, sorted;
+if (array == NULL || size < 2)
+return;
+for (node = getParent(size - 1); node != SIZE_MAX; node--)
+sift_down(array, size, node, size - 1);
+for (sorted = size - 1; sorted > 1; sorted--)
 {
-	int max = start;
-	int left = 2 * start + 1;
-	int right = 2 * start + 2;
-
-	if (!array || size < 2)
-		return;
-
-	if (left < end && array[left] > array[max])
-		max = left;
-
-	if (right < end && array[right] > array[max])
-		max = right;
-
-	if (start != max)
-	{
-		swap(&array[start], &array[max]);
-		print_array(array, size);
-		heapify(array, end, max, size);
-	}
+array[0] ^= array[sorted];
+array[sorted] ^= array[0];
+array[0] ^= array[sorted];
+print_array(array, size);
+sift_down(array, size, 0, sorted - 1);
 }
-
-/**
- * swap - Function that swaps two values
- *
- * @a: Fisrt value
- * @b: Second value
- * Return: 0
- */
-void swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
+array[0] ^= array[1];
+array[1] ^= array[0];
+array[0] ^= array[1];
+print_array(array, size);
 }
